947f7f82708fb0ca1cd1cdad76a10404ea9744d3
tituz175 <tobititus01@gmail.com>
2024-02-15 21:57:53 -0700

    task 3 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,5 +22,16 @@ class SessionAuth(Auth):
         if not user_id or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
-        SessionAuth.user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        _summary_
+
+        Returns:
+            str: _description_
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
